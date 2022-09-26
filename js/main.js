b14534870d23ef48b3a17ab69f3de4862341f3f8
sasashun <yasaisyakisyaki07@gmail.com>
2022-09-26 23:36:09 +0900

    PC版9割り完成

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(function($) {
+    $('.bg-switcher').bgSwitcher({
+        images: ['../images/header1.jpg','../images/header2.jpg','../images/header3.jpg','../images/header4.jpg','../images/header5.jpg'],
+        interval: 5000,
+        loop: true
+    });
+
+    particlesJS("courses", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},
+  "color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},
+  "image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,
+  "speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,
+  "sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,
+  "speed":6,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,
+  "rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,
+  "mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,
+  "duration":2,"opacity":8,"speed":3},"repulse":{"distance":16.240621041348632,"duration":0.4},"push":{"particles_nb":4},
+  "remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0);
+   stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; 
+   document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); 
+   update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) 
+    { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+    
+});
+
+$(document).ready(function(){
+    $('.fuwa').css('visibility','hidden');
+    $(window).scroll(function(){
+     var windowHeight = $(window).height(),
+         topWindow = $(window).scrollTop();
+     $('.fuwa').each(function(){
+      var objectPosition = $(this).offset().top;
+      if(topWindow > objectPosition - windowHeight + 200){
+       $(this).addClass("fuwatAnime");
+      }});
+    });
+});
+
+$(document).ready(function(){
+    $('.fuwa2').css('visibility','hidden');
+    $(window).scroll(function(){
+     var windowHeight = $(window).height(),
+         topWindow = $(window).scrollTop();
+     $('.fuwa2').each(function(){
+      var objectPosition = $(this).offset().top;
+      if(topWindow > objectPosition - windowHeight + 200){
+       $(this).addClass("fuwatAnime2");
+      }});
+    });
+});
